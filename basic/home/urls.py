54e9e182136cc1184b8d4feac44be9dc2f3d647f
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-18 14:03:14 +0530

    define api handler for create-docker

--- basic/home/urls.py
+++ basic/home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.ui_view, name='ui'),
+    path('create-docker', views.create_docker_api_handler, name='create_docker')
 ]
--- basic/home/views.py
+++ basic/home/views.py
@@ -1,33 +1,70 @@
 from django.shortcuts import render
 from django.conf import settings
-import os
-from django.views.decorators import csrf
-
-
+from django.http import HttpResponse, response
 from django.views.decorators.csrf import csrf_exempt
-@csrf_exempt #only for testing purpose
+
+import os, json
+
+
 def ui_view(request):
+    return render(request, 'index.html')
+
+
+def create_directory(name):
+    parent_dir = settings.BASE_DIR
+    project_dir = os.path.join(parent_dir, name)
+    try:
+        os.makedir(project_dir)
+        return project_dir
+    except:
+        print('Project with this name already exists')
+        return False
+
+def create_reponse_obj(status, message):
+    return {
+        'status': status,
+        'message': message
+    }
+
+
+@csrf_exempt # for test purpose only
+def create_docker_api_handler(request):
     if request.POST:
         # Fetch Data
         proj_name = request.POST.get('project-name').replace(' ', '-')
-        docker_content = request.POST.get('about')
+        docker_config = request.POST.get('about')
         
         # Create Project Directory
-        parent_dir = "/home/mohit/PROJ"
-        project_dir = os.path.join(parent_dir, proj_name)
-        os.mkdir(project_dir)
+        project_dir = create_directory(proj_name)
 
-        # Write Dockerfile
-        with open(os.path.join(project_dir, 'Dockerfile'), 'w') as file:
-            file.write(docker_content)
+        if project_dir:
+            # Write Dockerfile
+            docker_file_path = os.path.join(project_dir, 'Dockerfile')
+            with open(docker_file_path, 'w') as file:
+                file.write(docker_config)
+            data = create_reponse_obj('success', 'Successfully create dockerfile')
+            return HttpResponse(json.dumps(data), status=201)
 
-        # Scan Dockerfile
-        cmd = f'trivy config -f json -o {project_dir}/result.json {project_dir}'
-        os.system(cmd)
+        print('Error creating projectdir')
+        data = create_reponse_obj('fail', 'Project with this name already exists')
+        return HttpResponse(json.dumps(data), status=400)
 
-        # Fetch the Json file
-        json_file_path = os.path.join(project_dir, 'result.json')
-        with open(json_file_path, 'r') as file:
-            print(file.read())
+    print('Only POST method allowed')
+    data = create_reponse_obj('fail', 'Only POST method allowed')
+    return HttpResponse(json.dumps(data), status=400)
 
-    return render(request, 'index.html')
+
+
+
+
+"""
+def scandockerfile():
+    # Scan Dockerfile
+    cmd = f'trivy config -f json -o {project_dir}/result.json {project_dir}'
+    os.system(cmd)
+
+    # Fetch the Json file
+    json_file_path = os.path.join(project_dir, 'result.json')
+    with open(json_file_path, 'r') as file:
+        print(file.read())
+"""
