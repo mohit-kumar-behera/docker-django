dab8e13e44c05b9533e9ffff82cac9a57b82e2b8
ahtrahdis7 <sidharthabiki@gmail.com>
2021-09-21 17:21:38 +0530

    link: templates-ui to backend

--- ui/src/Components/TemplateList.js
+++ ui/src/Components/TemplateList.js
@@ -11,10 +11,10 @@ class TemplateList extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:5000/api/capabilities')
+        axios.get('http://127.0.0.1:5000/api/templates')
         .then( response => {
             console.log(response)
-            this.setState({posts: response.data})
+            this.setState({posts: response.data.message})
         })
         .catch(error => {
             console.log(error)
@@ -24,10 +24,34 @@ class TemplateList extends React.Component {
         const {posts} = this.state
         return (
         <div>
-        <p>existing templates</p>
+        {/* <p>existing templates</p> */}
         {
             posts.length ?
-            posts.map(post => <div key = {post.id}>{post.title}</div> ) :
+            posts.map(post => {
+
+                return (
+                    <div key = {post.TEMP_Id} style={{
+                        border: 1,
+                        borderStyle: 'dashed',
+                        padding: 5,
+                        margin: 5
+                    }}>
+                        <h3 style={{
+                            backgroundColor: 'grey',
+                            padding: 5,
+                            margin: 5,
+                        }}>{post.Name_of_template}</h3>
+                        <div>
+                            <h4>CAP_Id | Name_of_capability | description_of_capability</h4>
+                            {post.capabilities.map(cap => {
+                                return <div>
+                                        <h5>{cap.CAP_Id}. {cap.Name_of_capability} | {cap.description_of_capability}</h5>
+                                    </div>
+                            })}  
+                        </div>
+                    </div>
+                )
+            } ) :
             null
         }
 
