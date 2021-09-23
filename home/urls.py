2dd6169389b129be6c5a20213b9024699133f6ae
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-24 02:59:40 +0530

    completed handling create_template api call

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,8 @@ app_name = 'home'
 urlpatterns = [
     path('ui/', views.ui_view, name='ui'),
     path('create_project/', views.create_docker_api_handler, name='create_docker'),
-    path('scan/', views.scan_docker_file, name='scan'),
-    path('build/', views.build_docker_file, name='build'),
     path('create_compose_project/', views.compose_docker_api_handler, name='compose_docker'),
+    path('scan/', views.scan_docker_file, name='scan'),
     path('scan_compose_file/', views.scan_composed_docker_file, name='scan_composed_docker'),
+    path('build/', views.build_docker_file, name='build'),
 ]
